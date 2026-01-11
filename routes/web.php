f45714cb600fac01e243387dce119839f7ecb990
test <brucemclarenp4@gmail.com>
2026-01-12 01:38:23 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\PlaceController;
+use App\Http\Controllers\Public\PlacePublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//管理ページ
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,5 +24,19 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('places', PlaceController::class);
 });
+
+//公開ページ
+
+Route::get('/', [PlacePublicController::class, 'home'])->name('public.home');
+Route::get('/search', [PlacePublicController::class, 'search'])->name('public.search');
+
+Route::get('/categories/{category:slug}', [PlacePublicController::class, 'category'])
+    ->name('public.categories.show');
+
+Route::get('/tags/{tag:slug}', [PlacePublicController::class, 'tag'])
+    ->name('public.tags.show');
+
+Route::get('/places/{place:slug}', [PlacePublicController::class, 'show'])
+    ->name('public.places.show');
 
 require __DIR__.'/auth.php';
