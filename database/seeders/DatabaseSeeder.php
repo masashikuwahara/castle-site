0026199c3da723719888c9d43a0019348c3de485
test <brucemclarenp4@gmail.com>
2026-01-17 02:13:00 +0900

    seeder修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'test1234',
+            'name' => '',
+            'email' => '',
+            'password' => '',
         ]);
     }
 }
